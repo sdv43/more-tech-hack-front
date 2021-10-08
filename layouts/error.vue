9dc68abc40b204d818bb400f0895933e301d8b03
Dmitriy Selivestrov <q.selivestrov@gmail.com>
2021-10-08 22:25:48 +0300

    Добавлены основные layout'ы

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,15 +1,15 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
+  <v-container class='d-flex flex-column justify-center align-center mt-10'>
+    <h1 v-if='error.statusCode === 404'>
       {{ pageNotFound }}
     </h1>
     <h1 v-else>
       {{ otherError }}
     </h1>
-    <NuxtLink to="/">
+    <NuxtLink to='/'>
       Home page
     </NuxtLink>
-  </v-app>
+  </v-container>
 </template>
 
 <script>
@@ -21,13 +21,13 @@ export default {
       default: null
     }
   },
-  data () {
+  data() {
     return {
       pageNotFound: '404 Not Found',
       otherError: 'An error occurred'
     }
   },
-  head () {
+  head() {
     const title =
       this.error.statusCode === 404 ? this.pageNotFound : this.otherError
     return {
