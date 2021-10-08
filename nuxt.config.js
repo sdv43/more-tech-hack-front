084eda805ac2fda157816c529b76772881911dc0
Dmitriy Selivestrov <q.selivestrov@gmail.com>
2021-10-08 21:05:13 +0300

    Добавлены шрифты

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/font.scss',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -65,7 +66,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -75,6 +76,9 @@ export default {
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
+        },
+        light: {
+          primary: '#003ae5',
         }
       }
     }
