1586c0d681b8279791077e780e53be9b3e0cf3cd
Dmitriy Selivestrov <q.selivestrov@gmail.com>
2021-10-09 22:40:34 +0300

    Реализованы переходы по эвентам

--- components/WindowGame.vue
+++ components/WindowGame.vue
@@ -7,17 +7,37 @@
       elevation='2'
     >
       <v-container
+        v-if='event'
         :class='$style.card'
         class='d-flex flex-column align-stretch'
       >
         <p>
-          {{ step.text }}
+          {{ event.text }}
         </p>
 
-        <div>
-          controls
+        <div v-if='event.controls' class='my-8'>
+          <v-slider
+            v-model='controls.value'
+            label='Кол-во:'
+            thumb-label="always"
+            :min='event.controls.min'
+            :max='event.controls.max'
+          >
+            <template #append>
+              <v-text-field
+                v-model="controls.value"
+                hide-details
+                single-line
+                type="number"
+                class="mt-0 pt-0"
+                style="width: 30px"
+              ></v-text-field>
+            </template>
+          </v-slider>
         </div>
       </v-container>
+
+      <v-skeleton-loader v-else type='paragraph' />
     </v-card>
 
     <div
@@ -25,25 +45,37 @@
         up: () => $root.$emit("change-window", "stats")
       }'
     >
-      <div class='d-flex mb-4' :class='$style.actions'>
-        <div
-          v-for='btn in step.actions'
-          :key='btn.text'
-          class='flex-grow-1'
-          :class='$style.action'
-        >
-          <v-btn
-            color='primary'
-            block
+      <template v-if='event'>
+        <div class='d-flex mb-4' :class='$style.actions'>
+          <div
+            v-for='btn in event.actions'
+            :key='btn.text'
+            class='flex-grow-1'
+            :class='$style.action'
           >
-            {{ btn.text }}
+            <v-btn
+              block
+              color='primary'
+              :disabled='isActionsDisabled'
+              @click='clickAction(btn)'
+            >
+              {{ btn.text }}
+            </v-btn>
+          </div>
+        </div>
+
+        <div class='d-flex'>
+          <v-btn
+            class='flex-grow-1'
+            color='secondary'
+            :disabled='isActionsDisabled'
+          >
+            Пропустить ход
           </v-btn>
         </div>
-      </div>
+      </template>
 
-      <div class='d-flex'>
-        <v-btn class='flex-grow-1' color='secondary'>Пропустить ход</v-btn>
-      </div>
+      <v-skeleton-loader v-else type='actions' />
     </div>
 
   </v-container>
@@ -55,17 +87,88 @@ export default {
 
   data() {
     return {
-      step: {
-        text: 'Step text',
-        actions: [
-          { text: 'Взять кредит' },
-          { text: 'Не брать' }
-        ]
+      isActionsDisabled: false,
+      event: null,
+      controls: {
+        value: 0
       }
     }
   },
 
-  fetch(ctx) {
+  async mounted() {
+    this.event = await this.pullEvent()
+  },
+
+  methods: {
+    async clickAction(action) {
+      try {
+        this.isActionsDisabled = true
+        await this.pushEventReaction(action)
+        this.isActionsDisabled = false
+
+        this.$root.$emit('update-status')
+
+        this.event = null
+        this.event = await this.pullEvent()
+      } catch (e) {
+        alert(e.message)
+      }
+    },
+
+    pushEventReaction(action) {
+      // try {
+      //   let url
+      //
+      //   switch (action.text) {
+      //     case 'понял':
+      //       break
+      //     case 'принять':
+      //       url = '/api/event-ok'
+      //       break
+      //     case 'отклонить':
+      //       url = '/api/event-cancel'
+      //       break
+      //     case 'продать':
+      //       url = `/api/event-sell?value=${this.controls.value}`
+      //       break
+      //     case 'купить':
+      //       url = `/api/event-buy?value=${this.controls.value}`
+      //       break
+      //     default:
+      //       throw new Error('Unknown action')
+      //   }
+      //
+      //   if (url) {
+      //     await this.$axios.$get(url)
+      //   }
+      // } catch (e) {
+      //   console.error(e)
+      // }
+
+      return new Promise(resolve => setTimeout(resolve, 1000))
+    },
+
+    pullEvent() {
+      // try {
+      //   return this.$axios.$get('/api/get-next-event')
+      // } catch (e) {
+      //   console.error(e)
+      // }
+
+      return new Promise(resolve => {
+        setTimeout(() => resolve({
+          text: 'Из новостей узнали что завтра будет пиздец',
+          controls: {
+            min: 10,
+            max: 44,
+          },
+          actions: [
+            {text: 'Жить дальше'},
+            {text: 'Продать все'}
+          ]
+        }), 1000)
+      })
+    }
   }
 }
 </script>
